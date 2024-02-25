047085c8891cf4b45fe25c8c901358bc50206c73
Angelina Stiukhina <74776905+stSedge@users.noreply.github.com>
2024-02-25 15:24:09 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
-from api import db_api_class
+from api import api_class
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -15,14 +15,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-obj = db_api_class()
-
-obj.add_course(1, "course 0", "descriptions")
+obj = api_class()
 
 @app.get('/')
 def read_root():
-    obj.add_course(1, "course 0", "descriptions")
-    return HTMLResponse(123)
+    return 1
 
 
 @app.post("/api/signin")
@@ -33,30 +30,14 @@ def read_root(username : str, password : str):
 def read_root(username : str, name : str, password : str):
     return HTMLResponse(obj.sign_up(username, password, name))
 
-@app.get("/api/getName")
+@app.post("/api/createCourse")
+def read_root(id : int, name : str, describ : str):
+    return HTMLResponse(obj.add_course(id, name, describ))
+
+@app.post("/api/getName")
 def read_root(id : int):
     return HTMLResponse(obj.get_nick(id))
 
-@app.get("/api/getRef")
-def read_root(id : int):
-    return HTMLResponse(obj.get_ref(id))
-
-@app.get("/api/getCourses")
-def read_root(id : int):
-    return HTMLResponse(obj.get_courses(id))
-
-@app.get("/api/getAllCourses")
+@app.post("/api/getAllCourses")
 def read_root():
     return HTMLResponse(obj.get_all_courses())
-
-@app.get("/api/getImage")
-def read_root(id : int):
-    return HTMLResponse(obj.get_image(id))
-
-@app.post("/api/addCourse")
-def read_root(id : int, username : str, describtion : str):
-    return HTMLResponse(obj.sign_up(id, username, describtion))
-
-@app.post("/api/addTasktoCourse")
-def read_root(id : int, statement : str, maxMark : int, deadline : str):
-    return HTMLResponse(obj.sign_up(id, statement, maxMark, deadline))
