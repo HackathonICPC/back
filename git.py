085eaab9ed784bae918390f9ab748d825f34d3f4
Learpcs <34576126+Learpcs@users.noreply.github.com>
2024-02-25 13:10:11 +0400

    Update git.py

--- git.py
+++ git.py
@@ -1,6 +1,7 @@
 from github import Auth, Github
 from api import db_api_class
 
+#кто украдет токен тот лох
 auth = Auth.Token("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")
 org_name = "ICPCoursesORG"
 
@@ -10,7 +11,7 @@ class github_api_class:
 		self.org = self.g.get_organization(org_name)
 
 	def invite_user(self, task_id, user_id):
-		github_username = db_api_class.get_nick(user_id)
+		github_username = db_api_class.get_github_username(user_id)
 		self.g.get_repo(f"Task{task_id}_{user_id}").add_to_collaborators(github_username)
 		
 	def create_task(self, task_id, user_id):
@@ -18,7 +19,7 @@ class github_api_class:
 		self.invite_user(task_id, user_id)
 	
 	def get_url(self, task_id, user_id):
-		return 
+		return f"http://github.com/ICPCoursesORG/Task{task_id}_{user_id}"
 
 
 
